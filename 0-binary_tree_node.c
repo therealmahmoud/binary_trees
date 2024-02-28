f61fe7328c7d89f198bf3c1644ff3b1ac2858b04
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-28 06:21:11 +0200

    “commit”

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,5 +2,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	
+	binary_tree_t *newNode = NULL;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if(newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return(newNode);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
