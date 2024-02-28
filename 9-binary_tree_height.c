018ea5b7c485e170eb515e97e02cbb2900d2d159
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-28 22:04:32 +0200

    “commit”

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: Pointer to the root node of the binary tree.
+ *
+ * Return: The height of the binary tree.
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lheight, rheight;
