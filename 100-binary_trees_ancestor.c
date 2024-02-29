732c6f2a75f733b2267e1cde5087c705199e4143
therealmahmoud~ <moha91875460@gmail.com>
2024-02-29 21:28:33 +0200

    UPDATED

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,6 +13,8 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
+	if (!first || !second)
+		return (NULL);
 	if (first->parent == second)
 	{
 		binary_trees_ancestor(first->parent, second);
