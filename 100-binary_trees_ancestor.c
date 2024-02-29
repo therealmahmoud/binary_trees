a4145c5e06fbb41cda97895922f1a3868d7da8d1
therealmahmoud~ <moha91875460@gmail.com>
2024-02-29 22:00:59 +0200

    UPDATED

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -17,15 +17,16 @@ const binary_tree_t *second)
 		return (NULL);
 	if (first == second)
 		return ((binary_tree_t *)first);
+
 	if (first->parent == second || !second->parent ||
-		(!second->parent->parent && first->parent))
+	(!second->parent->parent && first->parent))
 	{
-			return (binary_trees_ancestor(first->parent, second));
+		return (binary_trees_ancestor(first->parent, second));
 	}
 	if (second->parent == first || !first->parent ||
-		(!first->parent->parent && second->parent))
+	(!first->parent->parent && second->parent))
 	{
-			return (binary_trees_ancestor(second->parent, first));
+		return (binary_trees_ancestor(second->parent, first));
 	}
 	return (binary_trees_ancestor(first->parent, second->parent));
 }
