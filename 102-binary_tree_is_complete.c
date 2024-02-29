3515cc74e20e60e30eafd28010e534f7d679373b
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-03-01 01:11:12 +0200

    “commit”

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+
+}
+
+/* Write a function that checks if a binary tree is complete
+
+Prototype: int binary_tree_is_complete(const binary_tree_t *tree);
+Where tree is a pointer to the root node of the tree to check
+If tree is NULL, your function must return 0 */
+
+
+/* alex@/tmp/binary_trees$ gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c 102-main.c 102-binary_tree_is_complete.c 0-binary_tree_node.c 3-binary_tree_delete.c -o 102-complete
+alex@/tmp/binary_trees$ ./102-complete
+       .-------(098)--.
+  .--(012)--.       (128)--.
+(010)     (054)          (402)
+Is 98 complete: 0
+Is 12 complete: 1
+       .-------(098)-------.
+  .--(012)--.         .--(128)--.
+(010)     (054)     (112)     (402)
+Is 98 complete: 1
+            .-------(098)-------.
+       .--(012)--.         .--(128)--.
+  .--(010)     (054)     (112)     (402)
+(008)
+Is 98 complete: 1
+            .------------(098)-------.
+       .--(012)-------.         .--(128)--.
+  .--(010)       .--(054)     (112)     (402)
+(008)          (023)
+Is 98 complete: 0 */
