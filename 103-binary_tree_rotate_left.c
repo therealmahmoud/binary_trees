92f26c8405277a7856c60518df95de37bd5e221e
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-03-01 02:20:46 +0200

    “commit”

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+
+
+/* Write a function that performs a left-rotation on a binary tree
+
+Prototype: binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+Where tree is a pointer to the root node of the tree to rotate
+Your function must return a pointer to the new root node of the tree once rotated */
+
+/* alex@/tmp/binary_trees$ gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c 103-binary_tree_rotate_left.c 103-main.c 0-binary_tree_node.c -o 103-rotl
+alex@/tmp/binary_trees$ ./103-rotl
+(098)--.
+     (128)--.
+          (402)
+Rotate-left 98
+  .--(128)--.
+(098)     (402)
+
+  .--(128)-------.
+(098)       .--(402)--.
+          (420)     (450)
+Rotate-left 128
+       .-------(402)--.
+  .--(128)--.       (450)
+(098)     (420) */
